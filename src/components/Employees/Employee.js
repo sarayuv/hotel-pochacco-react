f6993321ce15d0e5e0422c24fb7b972aecd8c142
Sarayu Vijayanagaram <147265985+sarayuv@users.noreply.github.com>
2024-07-08 15:29:51 -0400

    added pages

---
+++ src/components/Employees/Employee.js
@@ -0,0 +1,20 @@
+class Employee {
+  constructor(id, firstName, lastName, position, salary) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.position = position;
+    this.salary = salary;
+  }
+
+  print() {
+    console.log(`Employee ID: ${this.id}`);
+    console.log(`Employee First Name: ${this.firstName}`);
+    console.log(`Employee Last Name: ${this.lastName}`);
+    console.log(`Employee Position: ${this.position}`);
+    console.log(`Employee Salary: ${this.salary}`);
+    console.log("--------------------------------");
+  }
+}
+
+export default Employee;
