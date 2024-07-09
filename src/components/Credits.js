04369fb80bb34924b62c85dbfa07fd5a68c0d835
Sarayu Vijayanagaram <147265985+sarayuv@users.noreply.github.com>
2024-07-09 15:31:48 -0400

    added credits page

---
+++ src/components/Credits.js
@@ -0,0 +1,63 @@
+import React from "react";
+
+const Credits = () => {
+  return (
+    <div className="credits">
+      <h1>Credits</h1>
+      <p>This hotel management system was developed by Sarayu Vijayanagaram.</p>
+      <p>
+        Find more projects and code at{" "}
+        <a
+          href="https://github.com/sarayuv"
+          target="_blank"
+          rel="noopener noreffered"
+        >
+          github.com/sarayuv
+        </a>
+        .
+      </p>
+
+      <h3>Graphics Used:</h3>
+      <ul>
+        <li>
+          <a
+            href="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcReRmzInM07TeWHVfsH1C4Pv837GbuwTzyIL01KTNWGx5FbfkwE"
+            target="_blank"
+            rel="noopener noreffered"
+          >
+            https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcReRmzInM07TeWHVfsH1C4Pv837GbuwTzyIL01KTNWGx5FbfkwE
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://i.pinimg.com/564x/b9/94/b5/b994b523722c8b89340f4960e837e5ba.jpg"
+            target="_blank"
+            rel="noopener noreffered"
+          >
+            https://i.pinimg.com/564x/b9/94/b5/b994b523722c8b89340f4960e837e5ba.jpgh
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSWi05P7RDX83HOPdJ2mGVkJkvtDp29DRb_4JZrvOvrX_CaBJiQ"
+            target="_blank"
+            rel="noopener noreffered"
+          >
+            https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSWi05P7RDX83HOPdJ2mGVkJkvtDp29DRb_4JZrvOvrX_CaBJiQ
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQYskFEVu8ideNoCEd8j-abk7MhfMvReHZtu9uurvmy6kezd1uP"
+            target="_blank"
+            rel="noopener noreffered"
+          >
+            https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQYskFEVu8ideNoCEd8j-abk7MhfMvReHZtu9uurvmy6kezd1uP
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Credits;
